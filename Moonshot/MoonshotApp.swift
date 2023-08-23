743db63359e41b3058e4c3f14fcf5be58ecc5167
iVerkhov <vrkhv@iCloud.com>
2023-08-23 17:17:40 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Игорь Верхов on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
